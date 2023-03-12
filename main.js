011b4340bfb248016e71497ac2a0e349e3751bdb
hironoinoue <hai.nhajad@gmail.com>
2023-03-12 12:32:21 +0900

    camera

--- main.js
+++ main.js
@@ -1,29 +1,69 @@
-import * as THREE from "three";
+import * as THREE from "./build/three.module.js";
+import { OrbitControls } from "./jsm/controls/OrbitControls.js";
+let scene, camera, renderer;
+let sphere;
 
-let scene, camera,renderer;
+window.addEventListener("DOMContentLoaded", init);
 
-scene = new THREE.Scene();
+function init() {
+  const width = innerWidth;
+  const height = innerHeight;
 
-camera = new THREE.PerspectiveCamera(
-  50,
-  window.innerWidth/window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.set(0, 0, 500);
+  // シーン
+  scene = new THREE.Scene();
+  // カメラ（レクチャー部分）
+  //camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 3000);
+  //camera.position.z = 350;
+  const aspectRatio = width / height;
+  camera = new THREE.OrthographicCamera(500 * aspectRatio, -500 * aspectRatio, -500, 500, 0.1, 3000);
+  
 
-renderer = new THREE.WebGLRenderer({alpha: true});
-document.body.appendChild(renderer.domElement);
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.render(scene,camera);
 
-let ballGeometry = new THREE.SphereGeometry(100, 64,32);
-let ballMaterial = new THREE.MeshPhysicalMaterial();
-let ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
-scene.add(ballMesh);
+  // レンダラー
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(width, height);
+  document.body.appendChild(renderer.domElement);
 
-let directionalLight = new THREE.DirectionalLight(0xffffff, 2);
-directionalLight.position.set(1, 1, 1);
-scene.add(directionalLight);
+  // 座標軸を表示
+  const axes = new THREE.AxesHelper(1500);
+  axes.position.x = 0;
+  scene.add(axes); //x 軸は赤, y 軸は緑, z 軸は青
 
-renderer.render(scene,camera);
+  // ボックスを作成
+  const geometry = new THREE.SphereGeometry(200, 64, 32);
+  //const material = new THREE.MeshBasicMaterial({
+   // color: 0xc7ebb,
+    //wireframe: true,
+  //});
+
+  const material = new THREE.MeshNormalMaterial();
+  material.wireframe = true;
+  
+  sphere = new THREE.Mesh(geometry, material);
+  scene.add(sphere);
+
+  //マウス操作
+  new OrbitControls(camera, renderer.domElement);
+
+  window.addEventListener("resize", onWindowResize);
+  animate();
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  sphere.rotation.x += 0.01;
+  sphere.rotation.y += 0.01;
+
+  // レンダリング
+  renderer.render(scene, camera);
+}
+
+
+function onWindowResize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}
