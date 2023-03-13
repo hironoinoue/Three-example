a2fca43782d724ad72051548382cf5966721ac71
hironoinoue <hai.nhajad@gmail.com>
2023-03-14 07:08:59 +0900

    particles

--- main.js
+++ main.js
@@ -32,16 +32,52 @@ document.body.appendChild(renderer.domElement);
 /**
  * テクスチャ設定
  * /
-
 /**
  * パーティクルを作ってみよう
  */
-const particlesGeometry = new THREE.SphereGeometry(1, 16, 32);
+const textureLoader = new THREE.TextureLoader();
+const particlesTexture = textureLoader.load("./img/1.png");
+
+const particlesGeometry = new THREE.BufferGeometry();
+const count = 5000;
+
+const positionArray = new Float32Array(count * 3);
+const colorArray = new Float32Array(count * 3);
+
+for(let i = 0; i < count * 3; i++) {
+  positionArray[i] = (Math.random() - 0.5) * 10;
+  colorArray[i] = Math.random();
+}
+
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positionArray, 3),
+);
+particlesGeometry.setAttribute(
+  "color",
+  new THREE.BufferAttribute(colorArray, 3),
+);
+
+const cube = new THREE.Mesh(
+  new THREE.SphereGeometry(),
+  new THREE.MeshNormalMaterial()
+);
+
 const pointMaterial = new THREE.PointsMaterial({
-  size: 0.02,
+  size: 0.15,
+  transparent: true,
+  alphaMap: particlesTexture,
+  //alphaTest: 0.001,
+  //depthTest: false,
+  depthWrite: false,
+  vertexColors: true,
+  blending: THREE.AdditiveBlending,
 });
+//pointMaterial.map = particlesTexture;
+//pointMaterial.color.set("red");
+
 const particles = new THREE.Points(particlesGeometry, pointMaterial)
-scene.add(particles);
+scene.add(particles, cube);
 
 //マウス操作
 const controls = new OrbitControls(camera, renderer.domElement);
